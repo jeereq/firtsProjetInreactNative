cde96335c288bc889c21949994618f778e64cf75
jeereq <mingandajeereq@gmail.com>
2021-05-13 07:11:43 +0100

    first commit in develop

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Image, Platform } from "react-native";
+import { TabNavigator } from "react-navigation";
+import { PeopleScreen } from "./screens/PeopolScreen";
+import { DecisionScreen } from "./screens/DecisionScreen";
+import { RestaurantsScreen } from "./screens/RestaurantsScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	console.log("------------------------------------------------------------");
+	console.log(`RestaurantChooser starting on ${Platform.OS}`);
+	const platformOS = Platform.OS.toLowerCase();
+
+	return (
+		<View style={styles.container}>
+			<Text>hey i am jeereq</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center"
+	}
 });
